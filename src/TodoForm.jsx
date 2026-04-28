13875a4efea836c376c0733e60dd1ed61f01edba
negonch <negonch@gmail.com>
2026-04-28 15:40:09 -0700

    Lesson-04 is completed

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,11 +1,37 @@
-function TodoForm() {
+import { useRef } from "react";
+
+function TodoForm({ onAddTodo }) {
+  const inputRef = useRef();
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+
+    console.log("Event object:", event);
+    console.log("Event target:", event.target);
+    console.log("Input value:", event.target.todoTitle.value);
+
+    const todoTitle = event.target.todoTitle.value.trim();
+
+    if (todoTitle && todoTitle !== "") {
+      onAddTodo(todoTitle);
+      event.target.reset();
+      inputRef.current.focus();
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleAddTodo}>
+      {/* added an onSubmit event to the <form> to submit the input value to the page */}
       <label htmlFor="todoTitle">Todo</label>
-      <input type="text" id="todoTitle" />
-      <button type="submit" disabled>
-        Add Todo
-      </button>
+      <input
+        ref={inputRef}
+        type="text"
+        id="todoTitle"
+        name="todoTitle"
+        placeholder="Todo text"
+        required
+      />
+      <button type="submit">Add Todo</button>
+      {/* onClick={handleAddTodo} event doesn't work. Console: Cannot read properties of undefined (reading 'value') */}
     </form>
   );
 }
